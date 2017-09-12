29be7312d4f3f62a4d5a36dbae9fc65f6d59ea9e
Jonathan Dade <jonathandade@gmail.com>
2017-09-12 06:20:32 +0100

    some more changes

--- src/json-logging.js
+++ src/json-logging.js
@@ -1,9 +1,95 @@
-var jsonString = "";
+var jsonParts = [];
+var provider = "";
+var method = "";
+
+var splunkStart = '{ "time": %{time.now.sec}V, "host":"fastly-%{req.service_id}V", "sourcetype":"_json", "event" {';
+var splunkEnd = ' }';
+
+var start = "";
+var end = "";
 
 function init() {
-	jsonString = "1";
+  jsonString = "1";
 }
 
-function holder(){
-	alert("jsonString is: " + jsonString);
+function debugAlert() {
+  alert("provider is: " + provider + 
+    "\njsonString is: " + jsonString +
+    "\nmethod is: " + method);
+}
+
+function updateGenerated(){
+  out = ""
+  switch (provider) {
+    case "splunk":
+      out = out + splunkStart;
+      break;
+    case "sumologic":
+      out = out + sumologicStart;
+      break;
+    case "bigquery":
+      out = out + bigqueryStart;
+      break
+  }
+
+  out = out + jsonParts.join(", ");
+
+  out = out + 
+
+  document.getElementById("generated").innerHTML = out;
+}
+
+function updateProvider(){
+  // stash the new value
+  var prov = document.getElementById("provider").value;
+  // compare value against list
+  switch (prov) {
+    case "splunk":
+      provider = "splunk";
+      start = splunkStart;
+      end = splunkEnd;
+      break;
+    case "sumologic":
+      provider = "sumologic";
+      break;
+    case "bigquery":
+      provider = "bigquery";
+      break;
+    case "other":
+      provider = "other";
+      break;
+    case "bad":
+      alert("Bad choice. Try again.");
+
+  } 
+
+  // Debug out
+  debugAlert();
+
+  updateGenerated();
+}
+
+
+function updateMethod(){
+  // stash the new value
+  var val = document.getElementById("method").value;
+  // compare value against list
+  switch (val) {
+    case "UI":
+      method = "UI";
+      break;
+    case "API":
+      method = "API";
+      break;
+    case "bad":
+      alert("Blank chosen. Please try again.");
+    default:
+      method = "UI";
+      alert("Falling back to UI formatting");
+  } 
+
+  // Debug out
+  debugAlert();
+
+  updateGenerated();
 }
