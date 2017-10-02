a10ff17212e2ac2a90df3a5dc1126d9a5175bce4
Jonathan Dade <jonathandade@gmail.com>
2017-10-02 14:03:09 +0100

    Added guard for the upload tool.

--- tools/uploader.rb
+++ tools/uploader.rb
@@ -1,18 +1,68 @@
 #! /usr/bin/ruby
 
-require "fastly"
-require "yaml"
+require 'fastly'
+require 'yaml'
+require 'getoptlong'
 
-# Constants
-CREDS_FILE = ENV['HOME'] + "/.fastly/api-creds.yaml"
+# Uploads new configuration and pages to json-generator services
+module Uploader
+  # Default location for the credentials yaml
+  CREDS_FILE = ENV['HOME'] + '/.fastly/api-creds.yaml'
 
-creds = YAML.load(File.read(CREDS_FILE))
+  # Argument values
+  OPTS = GetoptLong.new(
+    ['--environment', '-e', GetoptLong::REQUIRED_ARGUMENT],
+    ['--help', '-h', GetoptLong::NO_ARGUMENT],
+    ['--creds-file', '-c', GetoptLong::OPTIONAL_ARGUMENT]
+  )
 
-f = Fastly.new(api_key: creds[:token])
+  # Module Variables
+  # @!visibility private
+  attr_accessor :creds, :fastly_env, :files_path
 
-puts f.authed?
-puts f.current_user
-puts f.current_customer
+  # Reads the credentials
+  def self.read_creds(creds_file = Uploader::CREDS_FILE)
+    creds_handle = File.open(creds_file, 'r')
+    self.creds = YAML.safe_load(creds_handle.read, [String], [])
+  rescue e
+    puts 'failed to read creds: ' + e.message
+  ensure
+    creds_handle.close
+  end
 
-#s = f.search_services(name: 'json-generate')
-#puts s
+  # Prints out a help message.
+  def self.show_help
+    message = 'Usage: uploader.rb [-h --help] '
+    message << '[-e --environment live|stage] '
+    message << '[-c --creds-file file] '
+    message << 'path-to-upload-files'
+    puts message
+    exit
+  end
+
+  # Runs over command arguments and sets values from them.
+  def self.process_arguments
+    Uploader::OPTS.each do |arg, value|
+      case arg
+      when '--help'
+        Uploader.show_help
+      when '--environment'
+        Uploader.fastly_env = value != 'live' ? :test : :live
+      when '--creds-file'
+        Uploader.creds = value
+      end
+    end
+  end
+
+  # Runs the upload
+  def self.run
+    if ARGV.empty?
+      puts 'Not enough Arguments'
+      show_help
+    end
+    Uploader.files_path = ARGV.pop
+    Uploader.process_arguments
+  end
+end
+
+Uploader.run if File.basename(__FILE__) == 'uploader.rb'
